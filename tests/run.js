d14f33852bcbf3e53aeb5d50a62e2e09b7cf3705
Matthew Francis Brunetti <zenflow87@gmail.com>
2016-01-13 19:17:52 -0500

    fix syntax error in test and prettify

--- tests/run.js
+++ tests/run.js
@@ -1,36 +1,30 @@
 var FastDownload = require('../');
-var url = 'http://nodejs.org/dist/v0.10.28/x64/node-v0.10.28-x64.msi';
-var options = {destFile: 'temp/node-v0.10.28-x64.msi', resumeFile: false, chunksAtOnce: 10};
 
-var on_error = function(error){
-    console.log('error', error);
-};
-var interval;
-var on_start = function(dl){
+new FastDownload('http://nodejs.org/dist/v0.10.28/x64/node-v0.10.28-x64.msi', {
+    destFile: 'node-v0.10.28-x64.msi',
+    resumeFile: false,
+    chunksAtOnce: 10
+}, function(error, dl){
+    if (error){
+        console.log('error', error);
+        return;
+    }
     console.log('headers', JSON.stringify(dl.headers));
-    interval = setInterval(function(){
+    var interval = setInterval(function(){
         console.log(
-            'size ' + (Math.floor(dl.size/1024/1024*100)/100) + 'MB\n'
-                + 'position ' + (Math.floor(dl.position/dl.size*10000)/100) + '%\n'
-                + 'downloaded ' + (Math.floor(dl.downloaded/dl.size*10000)/100) + '%\n'
+            'size ' + (dl.size/1024/1024).toFixed(2) + 'MB\n'
+            + 'position ' + (dl.position/dl.size*100).toFixed(2) + '%\n'
+            + 'downloaded ' + (dl.downloaded/dl.size*100).toFixed(2) + '%\n'
         );
     }, 200);
-};
-var on_end = function(){
-    clearInterval(interval);
-    console.log('end');
-};
-
-// - //
-/*new FastDownload(url, options)
-    .on('error', on_error)
-    .once('start', on_start)
-    .once('end', on_end);*/
-// - or - //
-new FastDownload(url, options, function(error, dl){
-    if (error){on_error(error); return;}
-    on_start(dl);
-    dl.once('end', on_end);
-    dl.once('done', function(){console.log('done');}
+    dl.once('error', function(){
+        console.log('error', error);
+    });
+    dl.once('end', function(){
+        clearInterval(interval);
+        console.log('end');
+    });
+    dl.once('done', function(){
+        console.log('done');
+    });
 });
-// - //
