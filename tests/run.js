528a58f8b407f3dfa1137ec375c6457b12d79f7e
Matthew Francis Brunetti <zenflow87@gmail.com>
2017-08-15 00:25:18 -0400

    Format all using eslint and prettier

--- tests/run.js
+++ tests/run.js
@@ -1,30 +1,40 @@
-var FastDownload = require('../');
+var FastDownload = require('../')
 
-new FastDownload('http://nodejs.org/dist/v0.10.28/x64/node-v0.10.28-x64.msi', {
+new FastDownload(
+  'http://nodejs.org/dist/v0.10.28/x64/node-v0.10.28-x64.msi',
+  {
     destFile: 'node-v0.10.28-x64.msi',
     resumeFile: false,
-    chunksAtOnce: 10
-}, function(error, dl){
-    if (error){
-        console.log('error', error);
-        return;
+    chunksAtOnce: 10,
+  },
+  function (error, dl) {
+    if (error) {
+      console.log('error', error)
+      return
     }
-    console.log('headers', JSON.stringify(dl.headers));
-    var interval = setInterval(function(){
-        console.log(
-            'size ' + (dl.size/1024/1024).toFixed(2) + 'MB\n'
-            + 'position ' + (dl.position/dl.size*100).toFixed(2) + '%\n'
-            + 'downloaded ' + (dl.downloaded/dl.size*100).toFixed(2) + '%\n'
-        );
-    }, 200);
-    dl.once('error', function(){
-        console.log('error', error);
-    });
-    dl.once('end', function(){
-        clearInterval(interval);
-        console.log('end');
-    });
-    dl.once('done', function(){
-        console.log('done');
-    });
-});
+    console.log('headers', JSON.stringify(dl.headers))
+    var interval = setInterval(function () {
+      console.log(
+        'size ' +
+          (dl.size / 1024 / 1024).toFixed(2) +
+          'MB\n' +
+          'position ' +
+          (dl.position / dl.size * 100).toFixed(2) +
+          '%\n' +
+          'downloaded ' +
+          (dl.downloaded / dl.size * 100).toFixed(2) +
+          '%\n'
+      )
+    }, 200)
+    dl.once('error', function () {
+      console.log('error', error)
+    })
+    dl.once('end', function () {
+      clearInterval(interval)
+      console.log('end')
+    })
+    dl.once('done', function () {
+      console.log('done')
+    })
+  }
+)
